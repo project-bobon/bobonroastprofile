13da81c1e12d7e4c652b6a545cb38f8ffc2563c7
Nathan Dao <nathan@guynathan.com>
2016-06-14 22:53:55 +0700

    refactoring

--- src/components/NewRoastForm.jsx
+++ src/components/NewRoastForm.jsx
@@ -64,49 +64,135 @@ class NewRoastForm extends React.Component {
               </div>
             </CardTitle>
 
-            <form className="bobon-form__new-roast" onSubmit={ this.props.onSubmit } onChange={ this.props.onChange } >
+            <form className="bobon-form__new-roast"
+              onSubmit={ this.props.onSubmit }
+              onChange={ this.props.onChange }
+            >
 
               <div className="bobon-textfield-wrapper bobon-util__full-width">
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                  <input className="mdl-textfield__input" type="text" id="beansName" name="beansName" value={ this.props.beansName }/>
-                  <label className="mdl-textfield__label" htmlFor="beansName">Bean's Name</label>
+                <div className="mdl-textfield mdl-js-textfield
+                                mdl-textfield--floating-label"
+                >
+                  <input
+                    className="mdl-textfield__input"
+                    type="text"
+                    id="beansName"
+                    name="beansName"
+                    value={ this.props.beansName }
+                  />
+
+                  <label
+                    className="mdl-textfield__label"
+                    htmlFor="beansName"
+                  >
+                    Bean's Name
+                  </label>
+
                 </div>
               </div>
 
               <div className="bobon-textfield-wrapper bobon-util__half-width">
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                  <input className="mdl-textfield__input" type="number" step="any" id="beansMoisture" name="beansMoisture" value={ this.props.beansMoisture }/>
-                  <label className="mdl-textfield__label" htmlFor="beansMoisture">Bean's Moisture (%)</label>
+                <div className="mdl-textfield mdl-js-textfield
+                                mdl-textfield--floating-label"
+                >
+                  <input
+                    className="mdl-textfield__input"
+                    type="number"
+                    step="any"
+                    id="beansMoisture"
+                    name="beansMoisture"
+                    value={ this.props.beansMoisture }
+                  />
+
+                  <label
+                    className="mdl-textfield__label"
+                    htmlFor="beansMoisture"
+                  >
+                    Bean's Moisture (%)
+                  </label>
+
                 </div>
               </div>
 
               <div className="bobon-textfield-wrapper bobon-util__half-width">
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                  <input className="mdl-textfield__input" type="number" step="any" id="batchSize" name="batchSize" value={ this.props.batchSize }/>
-                  <label className="mdl-textfield__label" htmlFor="batchSize">Batch Size (kg)</label>
+                <div className="mdl-textfield mdl-js-textfield
+                                mdl-textfield--floating-label"
+                >
+                  <input
+                    className="mdl-textfield__input"
+                    type="number"
+                    step="any"
+                    id="batchSize"
+                    name="batchSize"
+                    value={ this.props.batchSize }
+                  />
+
+                  <label
+                    className="mdl-textfield__label"
+                    htmlFor="batchSize"
+                  >
+                    Batch Size (kg)
+                  </label>
                 </div>
               </div>
 
               <div className="bobon-textfield-wrapper bobon-util__half-width">
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                  <input className="mdl-textfield__input" type="number" step="any" id="initialTemp" name="initialTemp" value={ this.props.initialTemp }/>
-                  <label className="mdl-textfield__label" htmlFor="batchSize">Initial Temperature (°C)</label>
+                <div className="mdl-textfield mdl-js-textfield
+                                mdl-textfield--floating-label"
+                >
+                  <input
+                    className="mdl-textfield__input"
+                    type="number"
+                    step="any"
+                    id="initialTemp"
+                    name="initialTemp"
+                    value={ this.props.initialTemp }
+                  />
+
+                  <label
+                    className="mdl-textfield__label"
+                    htmlFor="batchSize"
+                  >
+                    Initial Temperature (°C)
+                  </label>
+
                 </div>
               </div>
 
               <div className="bobon-textfield-wrapper bobon-util__full-width">
-                <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                  <textarea className="mdl-textfield__input" type="text" id="roastNote" name="roastNote" rows="5" value={ this.props.roastNote }></textarea>
-                  <label className="mdl-textfield__label" htmlFor="roastNote">Roasting Notes</label>
+                <div className="mdl-textfield mdl-js-textfield
+                                mdl-textfield--floating-label"
+                >
+                  <textarea
+                    className="mdl-textfield__input"
+                    type="text"
+                    id="roastNote"
+                    name="roastNote"
+                    rows="5"
+                    value={ this.props.roastNote }
+                  ></textarea>
+
+                  <label
+                    className="mdl-textfield__label"
+                    htmlFor="roastNote"
+                  >
+                    Roasting Notes
+                  </label>
                 </div>
               </div>
 
-              <input type="hidden" name="uid" id="uid" defaultValue={ this.props.uid } />
+              <input
+                type="hidden"
+                name="uid"
+                id="uid"
+                defaultValue={ this.props.uid }
+              />
 
               <div className="bobon-textfield-wrapper bobon-util__full-width">
                 { this.submitButton() }
                 { this.cancelButton() }
               </div>
+
             </form>
           </Card>
         </div>

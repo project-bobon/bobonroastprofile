92ddd843b807e115187f2a560b4c496bd0beb807
Nathan Dao <nathan@guynathan.com>
2016-06-16 21:26:55 +0700

    history

--- src/history.js
+++ src/history.js
@@ -1,5 +1,7 @@
 import { hashHistory } from 'react-router';
+//import { browserHistory } from 'react-router';
 
 const history = hashHistory;
+//const history = browserHistory;
 
 export default history;

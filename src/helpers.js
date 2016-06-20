8936bd297925a3f279b6d81bff8844394e556fe3
Nathan Dao <nathan@guynathan.com>
2016-06-20 20:22:22 +0700

    add display current measure system helper

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,6 @@
 import C from './constants';
 
+// Data is stored in database in metric form.
 export const metricWeight = (weight, unitSystem) => {
   if (unitSystem === C.IMPERIAL) {
     return weight * 0.453592;
@@ -13,5 +14,22 @@ export const metricTemp = (temp, unitSystem) => {
     return (temp - 32) / 1.8;
   } else {
     return temp;
+  }
+};
+
+// When displaying data, convert from metric to imperial when needed.
+export const displayTemp = (temp, unitSystem) => {
+  if (unitSystem === C.IMPERIAL) {
+    return (temp * 1.8 + 32).toFixed(2);
+  } else {
+    return temp.toFixed(2);
+  }
+};
+
+export const displayWeight = (weight, unitSystem) => {
+  if (unitSystem === C.IMPERIAL) {
+    return (weight / 0.454592).toFixed(2);
+  } else {
+    return weight.toFixed(2);
   }
 };

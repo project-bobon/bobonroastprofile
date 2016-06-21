f54184b537c260701c3ff7ea4f17e85300a1b8b7
Nathan Dao <nathan@guynathan.com>
2016-06-21 20:22:29 +0700

    update roast in progress icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -158,7 +158,6 @@ class Header extends React.Component {
             } }
         >
           <i className="material-icons">fiber_manual_record</i>
-          !
         </Button>
       );
     }
